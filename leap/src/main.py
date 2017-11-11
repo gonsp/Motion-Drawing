b81f202cc179b3de8707f40d121c75f2a76c2e7c
natbolon <natalie.bolonbrun@epfl.ch>
2017-11-11 13:57:47 +0100

    test

--- leap/src/main.py
+++ leap/src/main.py
@@ -4,8 +4,25 @@ import Leap
 
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
+class SampleListener(Leap.Listener):
+
+    def on_connect(self, controller):
+        print "Connected"
+
+    def on_frame(self, controller):
+        frame = controller.frame()
+
+        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
+            frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
+
 def main():
+
+    # Create a sample listener and controller
+    listener = SampleListener()
     controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
 
     # Keep this process running until Enter is pressed
     print "Press Enter to quit..."
@@ -13,6 +30,9 @@ def main():
         sys.stdin.readline()
     except KeyboardInterrupt:
         pass
+    finally:
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
 
 if __name__ == "__main__":
     main()
