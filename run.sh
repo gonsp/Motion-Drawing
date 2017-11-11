d59f8f2011fe13a7d0557e34d2cad28547707831
Gonzalo Solera <gonzalo1796@gmail.com>
2017-11-11 15:12:50 +0100

    Adding run script

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Leap motion service
-python2 leap/src/main.py
+python2 leap/src/main.py&
 
 # Trackpad service
+# Build
+# cd touchpad/TouchpadDrawing &&  xcodebuild
+# Execute
+./touchpad/TouchpadDrawing/build/Release/TouchpadDrawing&
 
 
 # Webserver with canvas
