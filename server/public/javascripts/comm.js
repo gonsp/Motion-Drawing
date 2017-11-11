4db56bfcdd1b13ff0ae2f2a21bfc5ef9c2b213dd
Pau Argelaguet <pau@outlook.com>
2017-11-12 00:00:19 +0100

    Getting ready for pointer

--- server/public/javascripts/comm.js
+++ server/public/javascripts/comm.js
@@ -1,5 +1,7 @@
 var socket = io();
 
-socket.on('leap-event', function (data) {
+socket.on('leap-event-client', function (data) {
     console.log('leap event');
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.fillRect(data.x, data.y, 1, 1);
 });
