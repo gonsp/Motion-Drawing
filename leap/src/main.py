7221377f5bb22afd78e38cce23172fc0e6fdbae5
natbolon <natalie.bolonbrun@epfl.ch>
2017-11-11 13:43:01 +0100

    test

--- leap/src/main.py
+++ leap/src/main.py
@@ -1,3 +1,18 @@
 import sys
 sys.path.insert(0, "../lib")
 import Leap
+
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+def main():
+    controller = Leap.Controller()
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
