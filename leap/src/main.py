49e13c0ac9dced84c515519f2fb95cdef63c33c1
natbolon <natalie.bolonbrun@epfl.ch>
2017-11-11 20:22:53 +0100

    calibration + scaling

--- leap/src/main.py
+++ leap/src/main.py
@@ -10,6 +10,8 @@ is_calibrated = False
 corners = []
 origin = None
 dimension = None
+is_hand_present = False
+
 
 
 class SampleListener(Leap.Listener):
@@ -17,24 +19,38 @@ class SampleListener(Leap.Listener):
         print "Connected"
 
     def on_frame(self, controller):
+        global is_hand_present
+
         frame = controller.frame()
         fingers = frame.fingers
         index = None
-        pos = None
+        tip_pos = None
+
+        if is_hand_present and fingers.is_empty:
+            is_hand_present = False
+            print "no hand"
+
+        elif not is_hand_present and not fingers.is_empty:
+            is_hand_present = True
+            print "HAND DETECTED"
+
+
         if not fingers.is_empty:
             index = fingers.finger_type(Leap.Finger.TYPE_INDEX)
             index = Leap.Finger(index[0])
-            pos = index.tip_position
+            tip_pos = index.tip_position
             if not is_calibrated:
-                calibrate(pos)
+                calibrate(tip_pos)
             else:
-                refresh(pos)
+                pos = refresh(tip_pos)
+                print pos
 
 
 def calibrate(pos):
     if select.select([sys.stdin, ], [], [], 0.0)[0]:
         sys.stdin.readline()
         corners.append(pos)
+        print "CALIBRATED CORNERS: %i" %len(corners)
 
     if len(corners) == 4:
         global is_calibrated
@@ -42,19 +58,27 @@ def calibrate(pos):
 
         global origin
         origin_x = corners[0][1]
-        origin_y = - corners[0][2]
+        origin_y = -corners[0][2]
         origin = [origin_x, origin_y]
+        print "ORIGIN X: %i, ORIGIN Y: %s" % (origin[0], origin_y)
 
         global dimension
         X = corners[2][1] - origin_x
         Y = -corners[2][2] - origin_y
         dimension = [X, Y]
+        print "DIM X: %i, DIM Y: %s" % (X, Y)
 
 
-def refresh(pos):
-    x = pos[1]
-    y = -pos[2]
-    
+def refresh(tip_pos):
+    #print tip_pos
+
+    x = tip_pos[1]
+    y = -tip_pos[2]
+    X = max(0,min((x - origin[0])/dimension[0],1))
+    Y = max(0,min((y-origin[1])/dimension[1],1))
+    return [round(X,2),round(Y,2)]
+
+
 
 
 """
