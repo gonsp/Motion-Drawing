76dd17e61606b9be1cad8e0febc1b080e5c1b63d
Pau Argelaguet <pau@outlook.com>
2017-11-12 01:52:08 +0100

    Added point

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/point', function(req, res, next) {
+  console.log("DATAPOINT");
+  res.send("")
+});
+
 module.exports = router;
